1bc540fe9e39fdba46d796489b175d251679be03
zzzzzzb <503684754@qq.com>
2017-04-13 22:33:35 +0800

    东林匿名信息平台

---
+++ js/publish.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2017/4/13.
+ */
+require.config({
+    path:{
+        "jquery" : "jquery-1.12.4"
+    }
+});
+define(["jquery"], function ($) {
+    return {
+        open : function (options) {
+            var defaultSettings = {
+                width : 400,
+                height : 300
+            }
+        }
+    }
+});
